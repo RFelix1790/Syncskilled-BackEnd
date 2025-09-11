942c3f242545fe2a05ef8ea3123a35ae28d32e33
Ricardo Félix <rmsfelix19@gmail.com>
2025-09-11 20:42:33 +0200

    user Schema

---
+++ models/UserSchema.js
@@ -0,0 +1,17 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    userName: { type: String, required: true, unique: true, trim: true },
+    email: { type: String, required: true, unique: tru, trim: true },
+    password: { type: String, required: true },
+    bio: { type: String, default: "" },
+    location: { type: String, required: true },
+    profilePhoto: { type: String, default: "" },
+    skillsToTeach: [{ type: Schema.Types.ObjectId, ref: "Skill" }],
+    skillsToLearn: [{ type: Schema.Types.ObjectId, ref: "Skill" }],
+  },
+  { timestamps: true }
+);
+const User = model("User", userSchema);
+export default User;
