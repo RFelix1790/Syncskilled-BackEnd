36257f3f3c0e4133bbe09988f05573a65f46ed4e
Kanu Kingsley <suge.kanu@gmail.com>
2025-09-13 16:58:13 +0200

    :sparkles:

--- routes/me.routes.js
+++ routes/me.routes.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import {refreshIfNeeded, requireAuth} from '../middlewares/auth.middleware.js'
-import { meService } from '../services/me.service.js';
+import {  getMeService,
+  patchMeService,
+  changeMyPasswordService,} from '../services/me.service.js';
 
 
 const router = express.Router()
+router.use(refreshIfNeeded, requireAuth);
 
-router.get('/', refreshIfNeeded, requireAuth,  meService)
+router.get('/', getMeService)
+router.patch('/', patchMeService);
+router.put('/password', changeMyPasswordService);
 
 export default router;
--- services/me.service.js
+++ services/me.service.js
@@ -1,3 +1,16 @@
-export function meService(req, res) {
+import User from '../models/UserSchema.js'
+import {comparePassword, hashPassword} from '../utils/security.js'
+
+
+export async function getMeService(req, res) {
   return res.json({ user: req.user });
+}
+
+export async function patchMeService(req,res){
+  try {
+    const { name, bio, locationCity, profilePhoto, username, email } = req.bod || {}
+    const updates = {}
+  } catch (error) {
+    
+  }
 }
